c2b34e88106c4a6be8cb45d66db527c8b21bcfe2
Collin Van Dyck <collinvandyck@gmail.com>
2023-05-19 23:54:07 -0400

    wip

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"math/rand"
+	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
@@ -12,26 +14,42 @@ func main() {
 	p.Start()
 }
 
-var _ tea.Model = model{}
-
 type model struct {
-	width  int
-	height int
-	ready  bool
+	board [][]bool
+	ready bool
 }
+
+type TickMsg time.Time
 
 // Init implements tea.Model
 func (m model) Init() tea.Cmd {
-	return nil
+	return doTick()
+}
+
+func doTick() tea.Cmd {
+	return tea.Tick(time.Second/60, func(t time.Time) tea.Msg {
+		return TickMsg(t)
+	})
 }
 
 // Update implements tea.Model
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
-		m.width = msg.Width
-		m.height = msg.Height
+		m.board = make([][]bool, msg.Height)
+		for i := range m.board {
+			m.board[i] = make([]bool, msg.Width)
+		}
+		for i := range m.board {
+			for j := range m.board[i] {
+				if rand.Float64() < 0.5 {
+					m.board[i][j] = true
+				}
+			}
+		}
 		m.ready = true
+	case TickMsg:
+		return m, doTick()
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "q", "esc", "ctrl+c":
@@ -46,24 +64,16 @@ func (m model) View() string {
 	if !m.ready {
 		return ""
 	}
-	var idx rune
 	buf := new(bytes.Buffer)
-loop:
-	for i := 0; i < m.height; i++ {
-		if i >= 20000000000 {
-			break loop
-		}
-		for j := 0; j < m.width; j++ {
-			if idx < 32 {
-				idx = 32
+	for i := range m.board {
+		for j := range m.board[i] {
+			if m.board[i][j] {
+				buf.WriteString("x")
+			} else {
+				buf.WriteString(" ")
 			}
-			if idx >= 127 {
-				idx = 32
-			}
-			buf.WriteRune(idx)
-			idx++
 		}
-		if i < m.height-1 {
+		if i != len(m.board)-1 {
 			buf.WriteString("\n")
 		}
 	}

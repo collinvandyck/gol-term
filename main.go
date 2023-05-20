59bfb710264f54ab22d5f125ca5d518df3f6d810
Collin Van Dyck <collinvandyck@gmail.com>
2023-05-19 23:41:45 -0400

    wip

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+
 	tea "github.com/charmbracelet/bubbletea"
 )
 
@@ -13,6 +15,9 @@ func main() {
 var _ tea.Model = model{}
 
 type model struct {
+	width  int
+	height int
+	ready  bool
 }
 
 // Init implements tea.Model
@@ -23,6 +28,10 @@ func (m model) Init() tea.Cmd {
 // Update implements tea.Model
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+		m.ready = true
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "q", "esc", "ctrl+c":
@@ -34,5 +43,29 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 // View implements tea.Model
 func (m model) View() string {
-	return "view"
+	if !m.ready {
+		return ""
+	}
+	var idx rune
+	buf := new(bytes.Buffer)
+loop:
+	for i := 0; i < m.height; i++ {
+		if i >= 20000000000 {
+			break loop
+		}
+		for j := 0; j < m.width; j++ {
+			if idx < 32 {
+				idx = 32
+			}
+			if idx >= 127 {
+				idx = 32
+			}
+			buf.WriteRune(idx)
+			idx++
+		}
+		if i < m.height-1 {
+			buf.WriteString("\n")
+		}
+	}
+	return buf.String()
 }
